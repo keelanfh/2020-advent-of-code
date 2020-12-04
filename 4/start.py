7bf9b5c6cc002c88e9f7d04927287fbf44a49a16
keelanfh <info@keelanfh.com>
2020-12-04 21:51:11 +0000

    day 4

---
+++ 4/start.py
@@ -0,0 +1,20 @@
+with open("input.txt") as f:
+    input = f.read()
+
+passports = input.split("\n\n")
+
+essential = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+
+valid_passports = 0
+
+for passport in passports:
+    kvs = passport.split()
+    keys = []
+    for kv in kvs:
+        k, v = kv.split(":")
+        keys.append(k)
+
+    if not essential.difference(keys):
+        valid_passports += 1
+
+print(valid_passports)
