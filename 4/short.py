66531313367f3e00e940d34d667b3425b89b7b2f
keelanfh <info@keelanfh.com>
2020-12-06 14:02:48 +0000

    Short versions of 4 and 5

--- 4/short.py
+++ 4/short.py
@@ -1,4 +1,3 @@
-e = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
-s = str.split
-print(sum(not e - {s(k, ":")[0] for k in s(p)}
-          for p in s(open("input.txt").read(), "\n\n")))
+e={"byr","iyr","eyr","hgt","hcl","ecl","pid"}
+s=str.split
+print(sum(not e-{s(k,":")[0]for k in s(p)}for p in s(open("4/input.txt").read(),"\n\n")))
