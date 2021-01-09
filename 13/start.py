3ff4328d40f1975507a4edb436477e0ffe7b8897
keelanfh <info@keelanfh.com>
2021-01-09 12:56:52 +0000

    13 part 2

--- 13/start.py
+++ 13/start.py
@@ -1,55 +1,43 @@
-from numpy import array
-import timeit
 from itertools import count
-from dis import dis
 
 with open("13/input.txt") as f:
     start, routes = f.readlines()
 
 start = int(start)
-routes_x = [int(x) if x != "x" else None for x in routes.split(",")]
-routes = [int(x) for x in routes.split(",") if x != "x"]
+routes = routes.split(",")
+routes = [int(x) if x != "x" else None for x in routes]
+routes_1 = [int(x) for x in routes if x]
 
 # Part 1
 
 
-def find_route():
+def find_route_1():
     for time in count(start):
-        for route in routes:
+        for route in routes_1:
             if not time % route:
                 return route * (time - start)
 
 
-print(find_route())
+print(find_route_1())
 
-# # Part 2 - not working yet!
+# Part 2
 
-# print(routes_x)
-
-# # print(509 - routes.index(509), 509)
-
-# routes_li = [route for index, route in enumerate(routes_x) if route]
-
-# routes = [(index, route) for index, route in enumerate(routes_x) if route]
-# routes.sort(key=lambda x: x[1])
-# routes.reverse()
-# print(routes)
-# # routes = array(routes)
+routes_2 = [(index, route) for index, route in enumerate(routes) if route]
 
 
-# def calculate():
-#     for time in count(max(routes_li) - routes_li.index(max(routes_li)), max(routes_li)):
-#         for index, route in routes:
-#             # If it doesn't match
-#             if (time + index) % route:
-#                 break
-#             # Otherwise
-#             else:
-#                 continue
-#         else:
-#             print(time)
-#             break
-#         print(time)
+def find_route_2():
+    time = 0
+    step = 1
+    index, route = routes_2.pop()
+    while True:
+        if not (time + index) % route:
+            step *= route
+            try:
+                index, route = routes_2.pop()
+            except IndexError:
+                return time
+        else:
+            time += step
 
 
-# print(timeit.timeit(calculate, number=5))
+print(find_route_2())
